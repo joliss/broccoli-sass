7612719ff513f4ed3774206bb9b5962e8501102c
Jaroslaw Zabiello <hipertracker@gmail.com>
2015-02-22 22:04:54 +0000

    fixed code using node-sass 2.0.1, updated dependent packages

--- index.js
+++ index.js
@@ -1,52 +1,62 @@
-var path = require('path')
-var mkdirp = require('mkdirp')
-var includePathSearcher = require('include-path-searcher')
-var CachingWriter = require('broccoli-caching-writer')
-var sass = require('node-sass')
-var _ = require('lodash')
-var Promise = require('rsvp').Promise
+var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp');
+var includePathSearcher = require('include-path-searcher');
+var CachingWriter = require('broccoli-caching-writer');
+var sass = require('node-sass');
+var _ = require('lodash');
+var Promise = require('rsvp').Promise;
 
-module.exports = SassCompiler
-SassCompiler.prototype = Object.create(CachingWriter.prototype)
-SassCompiler.prototype.constructor = SassCompiler
-function SassCompiler (inputTrees, inputFile, outputFile, options) {
-  if (!(this instanceof SassCompiler)) return new SassCompiler(inputTrees, inputFile, outputFile, options)
-  if (!Array.isArray(inputTrees)) throw new Error('Expected array for first argument - did you mean [tree] instead of tree?')
+module.exports = SassCompiler;
+SassCompiler.prototype = Object.create(CachingWriter.prototype);
+SassCompiler.prototype.constructor = SassCompiler;
+function SassCompiler(inputTrees, inputFile, outputFile, options) {
+    if (!(this instanceof SassCompiler))
+        return new SassCompiler(inputTrees, inputFile, outputFile, options);
 
-  CachingWriter.call(this, inputTrees, options)
+    if (!Array.isArray(inputTrees))
+        throw new Error('Expected array for first argument - did you mean [tree] instead of tree?');
 
-  this.inputFile = inputFile
-  this.outputFile = outputFile
-  options = options || {}
-  this.sassOptions = {
-    imagePath: options.imagePath,
-    outputStyle: options.outputStyle,
-    sourceComments: options.sourceComments,
-    sourceMap: options.sourceMap,
-    precision: options.precision
-  }
-}
+    CachingWriter.call(this, inputTrees, options);
 
-
-SassCompiler.prototype.updateCache = function(includePaths, destDir) {
-  var self = this
-
-  return new Promise(function(resolve, reject) {
-    var destFile = path.join(destDir, self.outputFile)
-    mkdirp.sync(path.dirname(destFile))
-
-    var sassOptions = {
-      file: includePathSearcher.findFileSync(self.inputFile, includePaths),
-      includePaths: includePaths,
-      outFile: destFile,
-      success: function() {
-        resolve(this)
-      },
-      error: function(err) {
-        reject(err)
-      }
+    this.inputFile = inputFile;
+    this.outputFile = outputFile;
+    options = options || {};
+    this.sassOptions = {
+        imagePath: options.imagePath,
+        outputStyle: options.outputStyle,
+        sourceComments: options.sourceComments,
+        sourceMap: options.sourceMap,
+        precision: options.precision
     }
-    _.merge(sassOptions, self.sassOptions)
-    sass.render(sassOptions)
-  })
 }
+
+SassCompiler.prototype.updateCache = function (includePaths, destDir) {
+    var self = this
+
+    return new Promise(function (resolve, reject) {
+        var destFile = path.join(destDir, self.outputFile);
+        mkdirp.sync(path.dirname(destFile));
+
+        var sassOptions = {
+            file: includePathSearcher.findFileSync(self.inputFile, includePaths),
+            includePaths: includePaths,
+            outFile: destFile,
+            success: function (res) {
+                fs.writeFile(destFile, res.css, function (err) {
+                    if (err != null)
+                        reject(err);
+                    else
+                        resolve(this);
+                });
+            },
+            error: function (err) {
+                reject(err)
+            }
+        };
+        _.merge(sassOptions, self.sassOptions);
+        sass.render(sassOptions);
+    })
+};
+
+
