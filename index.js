7671dccce5f56520f06568695dedc9a6206ffdd7
Ryan Tablada <ryan.tablada@gmail.com>
2015-12-30 18:25:00 -0600

    Allow configuration of custom importer

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ function SassCompiler (inputNodes, inputFile, outputFile, options) {
   this.renderSass = rsvp.denodeify(sass.render);
 
   this.sassOptions = {
+    importer: options.importer,
     functions: options.functions,
     indentedSyntax: options.indentedSyntax,
     omitSourceMapUrl: options.omitSourceMapUrl,
